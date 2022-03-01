f4f27a4047d522a8a255fb92ba4cedd2ea18bdaf
rafaelChelo <rafael@chelofinance.io>
2022-03-01 18:27:29 -0400

    First interface appears

---
+++ main.java
@@ -0,0 +1,8 @@
+import home.PaginaPrincipal;
+
+public class main {
+    public static void main(String[] args) {
+        PaginaPrincipal instance = new PaginaPrincipal();
+        instance.setVisible(true);
+    }
+}
