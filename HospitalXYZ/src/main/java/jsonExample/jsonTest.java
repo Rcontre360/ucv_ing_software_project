c463f8a2043f7aa9fc9c40858a555d5b86bd2c39
@acontrerasShokWorks <acontreras@shokworks.io>
2022-03-08 14:28:43 -0400

    Added jsonExample reader and writter

---
+++ HospitalXYZ/src/main/java/jsonExample/jsonTest.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+ 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+/**
+ *
+ * @author andres
+ */
+public class jsonTest {
+     public static void main(String args[]) {
+      //Creating a JSONObject object
+      //First Employee
+        /*JSONObject employeeDetails = new JSONObject();
+        employeeDetails.put("firstName", "Lokesh");
+        employeeDetails.put("lastName", "Gupta");
+        employeeDetails.put("website", "howtodoinjava.com");
+         
+        JSONObject employeeObject = new JSONObject(); 
+        employeeObject.put("employee", employeeDetails);
+         
+        //Second Employee
+        JSONObject employeeDetails2 = new JSONObject();
+        employeeDetails2.put("firstName", "Brian");
+        employeeDetails2.put("lastName", "Schultz");
+        employeeDetails2.put("website", "example.com");
+         
+        JSONObject employeeObject2 = new JSONObject(); 
+        employeeObject2.put("employee", employeeDetails2);
+         
+        //Add employees to list
+        JSONArray employeeList = new JSONArray();
+        employeeList.add(employeeObject);
+        employeeList.add(employeeObject2);
+         
+        //Write JSON file
+        try (FileWriter file = new FileWriter("employees.json")) {
+            //We can write any JSONArray or JSONObject instance to the file
+            file.write(employeeList.toJSONString()); 
+            file.flush();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        
+        JSONParser jsonParser = new JSONParser();
+         
+        try (FileReader reader = new FileReader("employees.json"))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+ 
+            JSONArray employeeList = (JSONArray) obj;
+            System.out.println(employeeList);
+             
+            //Iterate over employee array
+            employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
+ 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    private static void parseEmployeeObject(JSONObject employee) 
+    {
+        //Get employee object within list
+        JSONObject employeeObject = (JSONObject) employee.get("employee");
+         
+        //Get employee first name
+        String firstName = (String) employeeObject.get("firstName");    
+         
+        //Get employee last name
+        String lastName = (String) employeeObject.get("lastName");  
+        System.out.println(lastName +" "+ firstName);
+      
+    }
+}
