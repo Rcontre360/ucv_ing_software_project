1259dfdff0a9a4e42b3256c7ca2cfe9b92aaa421
@acontrerasShokWorks <acontreras@shokworks.io>
2022-03-13 20:14:22 -0400

    added universal functions

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,12 +1,23 @@
 import home.PaginaPrincipal;
 import utils.JsonWrapper ;
+import utils.db ;
 import org.json.simple.*;
+import InterfacesMédico.registrarDatosEnHistoria;
 
 public class main {
     public static void main(String[] args) {
         //PaginaPrincipal instance = new PaginaPrincipal();
         //instance.setVisible(true);
         JSONObject date = JsonWrapper.getDate("00000000");
+        JSONObject employeeDetails2 = new JSONObject();
+        employeeDetails2.put("ID", "Lokesha");
+        employeeDetails2.put("firstName", "Lokesha");
+        employeeDetails2.put("lastName", "Gupta");
+        employeeDetails2.put("website", "howtodoinjava.com");
+        db.setUniversal(employeeDetails2, "historiales_medicos2");
+        InterfacesMédico.registrarDatosEnHistoria.setPeso(4,"Brian");
+        InterfacesMédico.registrarDatosEnHistoria.setPesoTest(4,"Lokesha");
+        //
         System.out.println(date);
     }
 }
--- src/main/java/utils/db.java
+++ src/main/java/utils/db.java
@@ -2,7 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
- 
+
+/**
+ *
+ * @author andres
+ */
 package utils;
 
 import java.util.*;
@@ -15,8 +19,12 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-public class JsonWrapper {
-    public static String FILE_NAME = "formato.json";
+public class db {
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+    public static String FILE_NAME = "db.json";
      
     public static JSONArray getBoard() {
         JSONObject json = _getJson();
@@ -54,7 +62,60 @@ public class JsonWrapper {
         }
         return null;
     }
-   
+    
+    public static JSONObject getUniversal(String ID, String variableToModify){
+        JSONObject json = _getJson();
+        JSONArray historiales = (JSONArray) json.get(variableToModify);
+
+        for (Object obj : historiales) {
+            String historialID = (String) ((JSONObject)obj).get("ID");  
+            if (historialID.equals(ID)){
+                return ((JSONObject)obj);
+            }
+        }
+        return null;
+    }
+    
+    public static void setUniversal(JSONObject newObject, String variableToModify){
+        JSONObject json = _getJson();
+        JSONArray arrayToModify;
+        arrayToModify = (JSONArray) json.get(variableToModify);
+        int i=0;
+        boolean added = false;
+        System.out.println(arrayToModify);
+        if(arrayToModify != null){
+            for (Object obj : arrayToModify) {
+                System.out.println(((JSONObject)obj).get("ID"));
+                String ID = (String) ((JSONObject)obj).get("ID");                
+                if (ID.equals(newObject.get("ID"))){
+                    System.out.println("added si");
+                    arrayToModify.set(i, newObject);
+                    added = true;
+                }
+                i++;
+            }
+            if(added == false){
+                System.out.println("added no");
+                arrayToModify.add(newObject);
+            }
+            json.put(variableToModify, arrayToModify);
+        }else{
+            JSONArray historialNewArray = new JSONArray();
+            historialNewArray.add(newObject);
+            json.put(variableToModify, historialNewArray);
+        }
+
+        System.out.println(arrayToModify);
+        try {
+            FileWriter file;
+            file = new FileWriter("db.json");
+            file.write(json.toJSONString()); 
+            file.flush();
+ 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private static JSONObject _getJson(){
         JSONParser jsonParser = new JSONParser();
@@ -73,4 +134,6 @@ public class JsonWrapper {
 
         return obj;
     }
+
+
 }
