88c6a74248f1c431f3d9996b33cc062bbcf29081
rafaelChelo <rafael@chelofinance.io>
2022-03-13 20:59:52 -0400

    Added branch,date and doctor schemas

---
+++ src/main/java/utils/SchemaBranch.java
@@ -0,0 +1,42 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+ 
+package utils;
+
+import java.util.*;
+
+import utils.JsonWrapper;
+
+public class SchemaBranch {
+    
+    public SchemaBranch(String branchName){
+        
+    }
+
+    public String getDireccion(){
+
+    }
+
+    public void  setDireccion(direccion){
+
+    }
+
+    public int getLimite(){
+
+    }
+
+    public void  setLimite(limite){
+
+    }
+
+    public int getCapacidad(){
+
+    }
+
+    public void  setCapacidad(capacidad){
+
+    }
+}
---
+++ src/main/java/utils/SchemaDate.java
@@ -0,0 +1,23 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+ 
+package utils;
+
+import java.util.*;
+
+import utils.JsonWrapper;
+
+public class SchemaDate {
+    
+    public SchemaDate(String dateId){
+        
+    }
+
+    public String getFecha(){}
+    public void setFecha(fecha){}
+    public int getCodigo(){}
+    public void setCodigo(codigo){}
+}
