74675ad0fd843f3798657e9de647e22db6e97486
unknown <Rafax159>
2022-03-14 14:59:32 -0400

    Added .form files

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,13 +1,12 @@
 import home.PaginaPrincipal;
 import utils.JsonWrapper ;
-import utils.db ;
 import org.json.simple.*;
 import InterfacesMédico.registrarDatosEnHistoria;
 
 public class main {
     public static void main(String[] args) {
-        //PaginaPrincipal instance = new PaginaPrincipal();
-        //instance.setVisible(true);
+        PaginaPrincipal instance = new PaginaPrincipal();
+        instance.setVisible(true);
          JsonWrapper.loadJson();
         //System.out.println(date);
     }
