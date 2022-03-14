b712a1cccbac0fd719e8fda9ad0e9f99db4e902e
rafaelChelo <rafael@chelofinance.io>
2022-03-14 18:06:53 -0400

    SchemaDATE

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,20 +2,14 @@ import home.PaginaPrincipal;
 import org.json.simple.*;
 
 import schemas.SchemaBranch;
+import schemas.SchemaDate;
 import utils.JsonWrapper ;
 import InterfacesMédico.registrarDatosEnHistoria;
 
 public class main {
     public static void main(String[] args) {
-        //PaginaPrincipal instance = new PaginaPrincipal();
-        //instance.setVisible(true);
+        PaginaPrincipal instance = new PaginaPrincipal();
+        instance.setVisible(true);
         JsonWrapper.loadJson();
-        SchemaBranch branch = new SchemaBranch("Some shit");
-        branch.setDireccion("Some great direction");
-        branch.setLimite(10);
-        branch.setCapacidad(15);
-        System.out.println("COMMIT");
-        //branch.commit();
-        //System.out.println(date);
     }
 }
