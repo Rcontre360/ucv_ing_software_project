c0da11d1c524f54d7d7f9f15a4b91ccaedd03acf
YariCB <yarimucutu@gmail.com>
2022-03-15 22:21:43 -0400

    Schema Persona

---
+++ src/main/java/schemas/SchemaPersona.java
@@ -0,0 +1,109 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+ 
+package schemas;
+
+import java.util.*;
+
+import utils.JsonWrapper;
+
+public class SchemaPersona {
+    
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private char sexo;
+    private String lugarDeNacimiento;
+    private String fechaDeNacimiento;
+    private String estadoCivil;
+    private String direccionDeHabitacion;
+    private int telefono;
+    private String profesion;
+    private String ocupacion;    
+    
+    public SchemaPersona(){ }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getCedula() {
+        return cedula;
+    }
+    
+    public char getSexo(){
+        return sexo;
+    }
+    
+    public String getLugarDeNacimiento(){
+        return lugarDeNacimiento;
+    }
+    
+    public String getFechaDeNacimiento(){
+        return fechaDeNacimiento;
+    }
+    
+    public String getEstadoCivil(){
+        return estadoCivil;
+    }
+    
+    public String getDireccionDeHabitacion(){
+        return direccionDeHabitacion;
+    }
+    
+    public int getTelefono(){
+        return telefono;
+    }
+    
+    public String getProfesion(){
+        return profesion;
+    }
+    
+    public String getOcupacion(){
+        return ocupacion;
+    }
+    
+    public void setNombre(String _nombre){
+        nombre = _nombre;
+    }
+    
+    public void setCedula(String _cedula) {
+        cedula = _cedula;
+    }
+
+    public void setSexo(char _sexo) {
+        sexo = _sexo;
+    }
+
+    public void setLugarDeNacimiento(String _lugarDeNacimiento) {
+        lugarDeNacimiento = _lugarDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String _fechaDeNacimiento) {
+        fechaDeNacimiento = _fechaDeNacimiento;
+    }
+
+    public void setEstadoCivil(String _estadoCivil) {
+        estadoCivil = _estadoCivil;
+    }
+
+    public void setDireccionDeHabitacion(String _direccionDeHabitacion) {
+        direccionDeHabitacion = _direccionDeHabitacion;
+    }
+
+    public void setTelefono(int _telefono) {
+        telefono = _telefono;
+    }
+
+    public void setProfesion(String _profesion) {
+        profesion = _profesion;
+    }
+
+    public void setOcupacion(String _ocupacion) {
+        ocupacion = _ocupacion;
+    }
+    
+}
